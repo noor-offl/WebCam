2abc2b319b16cb11e18cd31ccddfcf522ec9a2d6
Noor <noor.offl@gmail.com>
2022-10-31 19:12:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webcam/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.webcam
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webcam.databinding.ActivityMainBinding
+
+private const val RC_CAMERA = 100
+private const val RC_RECORD_AUDIO = 101
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var mRequest: PermissionRequest? = null
+    private var cameraWhiteList = arrayListOf<String>()
+    private var micWhiteList = arrayListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding.webView) {
+            webChromeClient = setChromeClient()
+            webViewClient = WebViewClient()
+            configureWebSettings(settings)
+            loadUrl("https://www.google.com/search?q=test+mic")
+        }
+    }
+
+    private fun configureWebSettings(settings: WebSettings) {
+        settings.javaScriptEnabled = true
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.cacheMode = WebSettings.LOAD_NO_CACHE
+    }
+
+    private fun setChromeClient() = object : WebChromeClient() {
+        override fun onPermissionRequest(request: PermissionRequest?) {
+            for (r in request!!.resources) {
+                if (PermissionRequest.RESOURCE_VIDEO_CAPTURE == r) {
+                    mRequest = request
+                    askForPermission(
+                        arrayOf(Manifest.permission.CAMERA),
+                        "Camera",
+                        RC_CAMERA,
+                        cameraWhiteList
+                    )
+                }
+
+                if (PermissionRequest.RESOURCE_AUDIO_CAPTURE == r) {
+                    mRequest = request
+                    askForPermission(
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        "Microphone",
+                        RC_RECORD_AUDIO,
+                        micWhiteList
+                    )
+                }
+            }
+        }
+    }
+
+    private fun askForPermission(
+        permission: Array<String>,
+        hardware: String,
+        requestCode: Int,
+        whiteList: ArrayList<String>
+    ) {
+        if (whiteList.contains(mRequest!!.origin.toString())) requestPermissions(
+            arrayOf(Manifest.permission.RECORD_AUDIO), RC_RECORD_AUDIO
+        )
+        else {
+            val builder = AlertDialog.Builder(this@MainActivity).setTitle("$hardware permission")
+                .setMessage("${mRequest!!.origin} wants to access ${hardware.lowercase()}")
+                .setPositiveButton("Allow") { i, which ->
+                    requestPermissions(
+                        permission, requestCode
+                    )
+                }.setNegativeButton("Deny") { i, which ->
+                    i.dismiss()
+                }
+            builder.create().show()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (PackageManager.PERMISSION_GRANTED == grantResults[0]) {
+            if (RC_CAMERA == requestCode) {
+                mRequest!!.grant(mRequest!!.resources)
+                cameraWhiteList.add(mRequest!!.origin.toString())
+            }
+
+            if (RC_RECORD_AUDIO == requestCode) {
+                mRequest!!.grant(mRequest!!.resources)
+                micWhiteList.add(mRequest!!.origin.toString())
+            }
+            mRequest = null
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) binding.webView.goBack()
+        else super.onBackPressed()
+    }
+}
